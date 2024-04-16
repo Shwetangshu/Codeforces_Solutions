9395207ff647a470a6255f3957414bf4665f9be3
Shwetangshu Biswas <132643993+Shwetangshu@users.noreply.github.com>
2024-04-16 16:13:22 +0530

    Create 1926A_Vlad and the Best of Five.c

---
+++ 1926A_Vlad and the Best of Five.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        char str[6];
+        scanf("%s", str);
+        
+        int count_A = 0, count_B = 0;
+        for (int i = 0; i < 5; i++) {
+            if (str[i] == 'A') count_A++;
+            else if (str[i] == 'B') count_B++;
+        }
+        
+        printf("%c\n", (count_A > count_B) ? 'A' : 'B');
+    }
+    return 0;
+}
