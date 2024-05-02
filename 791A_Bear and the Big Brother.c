5ec378679fb14f3e2494b9af1aa67c4884aaea2d
Shwetangshu Biswas <132643993+Shwetangshu@users.noreply.github.com>
2024-05-02 22:22:21 +0530

    Create 791A_Bear and the Big Brother.c

---
+++ 791A_Bear and the Big Brother.c
@@ -0,0 +1,15 @@
+    #include <stdio.h>
+     
+    int main() {
+       int a,b;
+       scanf("%d %d",&a,&b);
+       int c=0;
+     while(a<=b)
+     {
+        a*=3;
+        b*=2;
+         c++;
+     }
+     printf("%d",c);
+     return 0;
+    }
